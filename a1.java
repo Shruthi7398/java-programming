32d3e15aa5a734ba0ed250908ad707d2f167c098
Shruthi7398 <41152363+Shruthi7398@users.noreply.github.com>
2018-07-20 21:49:52 +0530

    Add files via upload

---
+++ a1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class a1{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n==0)
+System.out.println("Zero");
+else if(n>0)
+System.out.println("Positive");
+else 
+System.out.println("Negative");
+}}
